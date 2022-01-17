e97bb8cefb2bbc5aaa01969bf902ca45104c4b0e
banzak1 <leosantanabr@gmail.com>
2022-01-17 14:19:11 -0300

    inicializando projeto de financas do desafio da solinftec

---
+++ back_stock/src/test/java/com/stock_api/back_stock/BackStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock_api.back_stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ back_user/src/test/java/com/user_api/back_user/BackUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user_api.back_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
